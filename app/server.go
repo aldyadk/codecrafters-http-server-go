2e0bf7fdf813298d9f513091fcbbe5532c165e4a
aldy.andika <aldy.andika@gmail.com>
2024-07-12 10:50:32 +0000

    goroutine

--- app/server.go
+++ app/server.go
@@ -33,23 +33,11 @@ func parsePath(path string) []string {
 
 }
 
-func main() {
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+func handleRequest(conn net.Conn) {
 	req := make([]byte, 1024)
 	conn.Read(req)
-	method, path, headers, _ := parseRequest(string(req))
+	request := string(req)
+	method, path, headers, _ := parseRequest(string(request))
 	paths := parsePath(path)
 	var pathA, pathB string
 	pathA = paths[1]
@@ -70,4 +58,21 @@ func main() {
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
+}
+
+func main() {
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	go handleRequest(conn)
 }
