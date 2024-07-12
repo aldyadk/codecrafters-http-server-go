05bdcadf95bf6a6cec2e50201e3e2654f21c0b39
aldy.andika <aldy.andika@gmail.com>
2024-07-12 10:00:06 +0000

    add listener for /

--- app/server.go
+++ app/server.go
@@ -6,20 +6,35 @@ import (
 	"os"
 )
 
+func parseRequest(request string)(string,string){
+	var method, path string
+	fmt.Sscanf(request, "%s %s HTTP/1.1", &method, &path)
+	return method, path
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
+
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	req := make([]byte, 1024)
+	conn.Read(req)
+	method, path := parseRequest(string(req))
+	fmt.Println("Method:", method)
+	fmt.Println("Path:", path)
+
+	if method == "GET" && path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 }
