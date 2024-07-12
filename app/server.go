f24dbc3dade9d7fbe4529fd335c3c17f74723bb1
aldy.andika <aldy.andika@gmail.com>
2024-07-12 10:42:06 +0000

    handle user agent

--- app/server.go
+++ app/server.go
@@ -7,10 +7,25 @@ import (
 	"strings"
 )
 
-func parseRequest(request string) (string, string) {
-	var method, path string
-	fmt.Sscanf(request, "%s %s HTTP/1.1", &method, &path)
-	return method, path
+func parseRequest(request string) (string, string, map[string]string, string) {
+	var method, path, body string
+	headers := make(map[string]string)
+	lines := strings.Split(request, "\r\n")
+	fmt.Sscanf(lines[0], "%s %s HTTP/1.1", &method, &path)
+
+	var i int
+	for i = 1; i < len(lines); i++ {
+		line := lines[i]
+		if line == "" {
+			break
+		}
+		headerParts := strings.SplitN(line, ": ", 2)
+		headers[headerParts[0]] = headerParts[1]
+	}
+
+	body = strings.Join(lines[i+1:], "\r\n")
+
+	return method, path, headers, body
 }
 
 func parsePath(path string) []string {
@@ -34,7 +49,7 @@ func main() {
 	}
 	req := make([]byte, 1024)
 	conn.Read(req)
-	method, path := parseRequest(string(req))
+	method, path, headers, _ := parseRequest(string(req))
 	paths := parsePath(path)
 	var pathA, pathB string
 	pathA = paths[1]
@@ -42,10 +57,15 @@ func main() {
 		pathB = paths[2]
 	}
 
-	if method == "GET" && path == "/" {
+	if method == "GET" && pathA == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if method == "GET" && pathA == "user-agent" {
+		if headers["User-Agent"] != "" {
+			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(headers["User-Agent"]), headers["User-Agent"])))
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
 	} else if method == "GET" && pathA == "echo" && pathB != "" {
-		fmt.Println(pathB)
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(pathB), pathB)))
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
