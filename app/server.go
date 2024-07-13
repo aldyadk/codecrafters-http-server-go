ba8b04e837cccf99beaf7fbd98c89e8a817c856a
aldy.andika <aldy.andika@gmail.com>
2024-07-13 13:09:34 +0000

    test

--- app/server.go
+++ app/server.go
@@ -80,6 +80,7 @@ func handleConnection(conn net.Conn) {
 	} else if method == "GET" && pathA == "echo" && pathB != "" {
 		body := []byte(body)
 		compressedBody, err := gzipCompress(body)
+		fmt.Println(compressedBody)
 		if err != nil {
 			log.Fatal(err)
 		}
