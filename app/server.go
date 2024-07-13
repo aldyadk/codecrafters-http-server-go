9e08029925f1d48b2fbc905696ecfcd9737f5391
aldy.andika <aldy.andika@gmail.com>
2024-07-13 13:26:31 +0000

    test

--- app/server.go
+++ app/server.go
@@ -87,7 +87,7 @@ func handleConnection(conn net.Conn) {
 
 		if headers["Accept-Encoding"] != "" && strings.Contains(headers["Accept-Encoding"], "gzip") {
 			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: %s\r\nContent-Length: %d\r\n\r\n", "gzip", len(compressedBody))))
-			conn.Write(compressedBody)
+			conn.Write([]byte(compressedBody))
 			conn.Close()
 		} else {
 			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(pathB), pathB)))
